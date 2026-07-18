f553a059cf38dad778da1b2d2d34753d088759c0
sadegh <s.saati@nizek.com>
2026-07-18 18:35:54 +0330

    search box added

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { Loading } from "@/src/common/presentation/Loading";
 import MovieItemCell from "@/src/features/home/MovieItem";
+import SearchBox from "@/src/features/home/SearchBox";
 import { useMovieList } from "@/src/network/api/GetMovieListApi";
 import { useRouter } from "next/navigation";
 
@@ -16,25 +17,20 @@ export default function Home() {
     return <h1>Error</h1>
 
   if (data)
-    return <div style={{
-      "display": "grid",
-      gridTemplateColumns: "repeat(auto-fill, minmax(200px, 1fr))",
-      "gap": "16px"
-    }}>
-      {
-        data.map((items) => {
-          return <MovieItemCell key={items.imdbID} item={items} />
-        })
-      }
+    return <div>
+      <SearchBox />
+
+      <div style={{
+        "display": "grid",
+        gridTemplateColumns: "repeat(auto-fill, minmax(200px, 1fr))",
+        "gap": "16px"
+      }}>
+        {
+          data.map((items) => {
+            return <MovieItemCell key={items.imdbID} item={items} />
+          })
+        }
+      </div>
     </div>
-  return (
 
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-
-      <button style={{ color: "white" }} title="click me" onClick={() => {
-        router.push("/innerPage")
-      }}>click me</button>
-
-    </div>
-  );
 }
---
+++ src/features/home/SearchBox.tsx
@@ -0,0 +1,4 @@
+const SearchBox = () => {
+    return <input type="text" placeholder="type here to search" />
+}
+export default SearchBox
